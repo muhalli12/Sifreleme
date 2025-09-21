150bf8f642cba55966d76988ecae5c66c67d6dc5
Muhammed Ali <muhammedalicinar1812@gmail.com>
2025-09-21 11:38:44 +0300

    Sifreleme
    C# kullanarak bit sifreleme ile yapılan bir program. Sifreleme yöntemi ROT15

---
+++ SifreliResim/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SifreliResim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Out = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSelect.Location = new System.Drawing.Point(190, 377);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(110, 38);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Dosya Seç";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEncrypt.Location = new System.Drawing.Point(327, 377);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(110, 38);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Şifrele";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDecrypt.Location = new System.Drawing.Point(464, 377);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(110, 36);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Çöz";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(132, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 303);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(190, 331);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(384, 20);
+            this.txtFilePath.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Out
+            // 
+            this.Out.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Out.ForeColor = System.Drawing.Color.Red;
+            this.Out.Location = new System.Drawing.Point(677, 456);
+            this.Out.Name = "Out";
+            this.Out.Size = new System.Drawing.Size(89, 43);
+            this.Out.TabIndex = 5;
+            this.Out.Text = "ÇIKIŞ";
+            this.Out.UseVisualStyleBackColor = true;
+            this.Out.Click += new System.EventHandler(this.Out_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(778, 511);
+            this.Controls.Add(this.Out);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "Sifreleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button Out;
+    }
+}
+
---
+++ SifreliResim/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace SifreliResim
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFile = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png;*.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = ofd.FileName;
+                txtFilePath.Text = selectedFile;//TextBox'ta seçilen dosya yolunu gösteriyor
+                pictureBox1.ImageLocation = selectedFile;//Resmi ekranda ön izleme ile gösteriyor
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFile)) return;// Kullanıcı bir dosya seçmiş mi seçmemiş mi kontrol ediyor?
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Şifreli Dosya|*.enc";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                EncryptFile(selectedFile, sfd.FileName, 15);//Şifreleme burada yapılıyor
+                MessageBox.Show("Dosya şifrelendi!");
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Şifreli Dosya|*.enc";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Resim Dosyası|*.jpg;*.png;*.bmp";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    DecryptFile(ofd.FileName, sfd.FileName, 15);//Şifre burada çözülüyor
+                    MessageBox.Show("Dosya çözüldü!");
+                    pictureBox1.ImageLocation = sfd.FileName;
+                }
+            }
+        }
+        private void EncryptFile(string inputFile, string outputFile, byte rot)
+        {
+            byte[] data = File.ReadAllBytes(inputFile);//Dosyayı tümüyle Byte dizisi olarak okur
+            for (int i = 0; i < data.Length; i++)
+                data[i] = (byte)((data[i] + rot) % 256);//ROT15 olarark kaydırır
+
+            File.WriteAllBytes(outputFile, data);// Yeni Byte dizisini kaydeder
+        }
+
+        private void DecryptFile(string inputFile, string outputFile, byte rot)
+        {
+            byte[] data = File.ReadAllBytes(inputFile);
+            for (int i = 0; i < data.Length; i++)
+                data[i] = (byte)((data[i] - rot + 256) % 256);//Her byte - yönde kaydırır
+
+            File.WriteAllBytes(outputFile, data);// Orijinal resim bytlarını geri yazar
+        }
+
+        private void Out_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
